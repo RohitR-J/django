e2f407976659bd9fd01e65fa0cbfc0067fe9c324
Rohit Raj <75240698+RohitR-J@users.noreply.github.com>
2022-03-04 12:34:28 +0530

    Update urls.py

--- mashupcommrz/mashupcommrz/urls.py
+++ mashupcommrz/mashupcommrz/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('adminpannel/', include('adminpannel.urls')),
+    path('', RedirectView.as_view(url='adminpannel/'))
     path('customer/', include('customer.urls')),
     path('customerapi/', include('customerapi.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
